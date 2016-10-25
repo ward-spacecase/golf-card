2d54cfdc6a556e5195fcb7a069bf0d190faf8704
ward-spacecase <ward.spacecase@gmail.com>
2016-10-24 21:53:43 -0600

    Geolocation API

--- src/js/scorecard.js
+++ src/js/scorecard.js
@@ -1,6 +1,37 @@
 $(window).load(function() {
 
-    setTimeout(function() {
+    geoFindMe();
+
         $( '#myModal' ).modal('show');
-    }, 500);
+        $('.modal-body').html('<i class="fa fa-spinner fa-pulse fa-4x"></i>');
+
 });
+
+function geoFindMe() {
+    var output = $('.modal-body');
+
+    if (!navigator.geolocation){
+        output.html("<p>Geolocation is not supported by your browser</p>");
+        return;
+    }
+
+    function success(position) {
+        var latitude  = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+        output.html('<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>');
+
+    };
+
+    function error() {
+        output.html("Unable to retrieve your location");
+    };
+
+    output.html("<p>Locating…</p>");
+
+    navigator.geolocation.getCurrentPosition(success, error);
+}
+
+
+
+
