56dffd3e0fdc79f3c7f28b4af2c008892951e158
ward-spacecase <ward.spacecase@gmail.com>
2016-10-26 10:10:31 -0600

    Geolocation API

--- src/js/scorecard.js
+++ src/js/scorecard.js
@@ -19,17 +19,46 @@ function geoFindMe() {
         var latitude  = position.coords.latitude;
         var longitude = position.coords.longitude;
 
-        output.html('<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>');
+            var domUpdate = golfHttp(latitude, longitude);
+
+                console.log(domUpdate);
+                output.html();
 
     };
 
     function error() {
         output.html("Unable to retrieve your location");
+
     };
 
-    output.html("<p>Locating…</p>");
-
     navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function golfHttp(lat, long) {
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+
+        console.log(this.readyState + '  ' + this.status);
+
+        if(this.readyState == 4 && this.status == 200) {
+            console.log(xhr.responseText);
+            var jsonReply = JSON.parse(xhr.responseText);
+
+           if(jsonReply.meta.courses.total <= 0) {
+               return '<h1>No courses found</h1> <form><label for="city">City: </label><input type="text" name="city">' +
+                   ' <button type="submit" class="btn btn-primary"></button></form>';
+           }
+        }
+    };
+    xhr.open("POST", "https://golf-courses-api.herokuapp.com/courses/search", false);
+    xhr.setRequestHeader('contentType', 'application/json');
+        xhr.send({
+            latitude: lat,
+            longitude: long,
+            radius: '50',
+        });
+
 }
 
 
