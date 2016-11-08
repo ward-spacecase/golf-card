709175c47065f82c157897fd6b5adae87c2a6d53
ward-spacecase <ward.spacecase@gmail.com>
2016-11-08 09:53:24 -0700

    Google Places API & started Players

--- src/js/chooseCourse.js
+++ src/js/chooseCourse.js
@@ -7,6 +7,9 @@ $(window).load(function() {
 
 });
 
+var coursesGlobal;
+var globalCourse;
+var locationGlobal;
 function geoFindMe() {
     var output = $('.modal-body');
 
@@ -24,11 +27,12 @@ function geoFindMe() {
 
             }
                 output.html();
-
+//open weather map .org api
     }
 
     function error() {
-        output.html("Unable to retrieve your location");
+        output.html("Unable to retrieve your location. You have failed me. Please allow location services and refresh the page.");
+        $('.modal-content').css('background-color', 'red');
 
     }
 
@@ -36,46 +40,134 @@ function geoFindMe() {
 }
 
 function golfHttp(lat, long) {
-    var xhr = new XMLHttpRequest();
-
-    var formData = new FormData();
-    formData.append('city', 'Lehi');
-    formData.append('latitude', lat);
-    formData.append('longitude', long);
-    formData.append('radius', 50);
-
-    xhr.onreadystatechange = function () {
-
-        console.log(this.readyState + '  ' + this.status);
-
-        if(this.readyState == 4 && this.status == 200) {
-            console.log(xhr.responseText);
-            var jsonReply = JSON.parse(xhr.responseText);
-
-            $("body").data( "userData", jsonReply);     //send response to new page
 
 
-            $( '.modal-body' ).html('<h1>' + jsonReply.course.name +
-                '</h1><hr class="style18"><h2>' + jsonReply.course.city + ', ' + jsonReply.course.state_or_province +
-                '</h2><h3>' + jsonReply.course.addr_1 + '</h3>');
+    var location = new google.maps.LatLng(lat,long);
+    locationGlobal = location;
+
+    var formData = {latitude: lat, longitude: long, radius: 100};
+
+    $.post("http://golf-courses-api.herokuapp.com/courses/", formData, function (data) {
+
+        data = JSON.parse(data);
+        console.log(data.courses);
+
+
+
+            $('.modal-body').html('<select name="courses" id="courses-select"></select>');
+            coursesGlobal = data;
+
+        for(var courses = 0; courses < data.courses.length; courses++) {
+            $('#courses-select').append('<option value="' + courses + '">'+ data.courses[courses].name + '</option>');
+        }
+
 
             $('#choose').removeClass('disabled btn-default', 1000, 'swing');
-            $('#choose').addClass('btn-success')
+            $('#choose').addClass('btn-success');
 
-        }
+        $('#choose').click(function () {nextSlide(data.courses[$('#courses-select option:selected').val()],
+            location)});
+
+    });
+
+}
+function nextSlide(course,locationForSearch) {
+
+    globalCourse = course;
+    $( '.modal-body' ).html('<h1>' + course.name +
+        '</h1><hr class="style18"><h2>' + course.city + ', ' + course.state_or_province +
+        '</h2><h3>' + course.addr_1 + '</h3>');
+    $('.modal-body').append('<div class="placeholder" style="height: 500px"></div>');
+
+    weatherAPI();
+
+    var request = {
+        location: locationForSearch,
+        radius: '50000',
+        name: course.name,
+        rankby: 'distance'
     };
-    xhr.open("GET", "https://golf-courses-api.herokuapp.com/courses/11819", false);
-    // xhr.open("POST", "https://golf-courses-api.herokuapp.com/courses/search", false);
-    xhr.setRequestHeader('contentType', 'application/json');
 
-        xhr.send();
-        // xhr.send(formData);
+    var service = new google.maps.places.PlacesService($('.hold').get(0));
+    service.nearbySearch(request, callback);
+
+    var photoURL;
+    var place;
+
+    function callback(results, status) {
+        $('.placeholder').remove();
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+            console.log(results);
+            place = results[0];
+                if(place.photos.length > 0) {
+                    photoURL = place.photos[0].getUrl({'maxWidth': 400});
+                    $('.modal-body').append('<img src="' + photoURL + '" width="90%">');
+                } else {
+                    console.log('sorry, No photos here');
+                }
+        } else {
+            console.log('Couldnt call places api');
+        }
+
+    }
+    $('.modal-title').html('Is this the course you want?');
+    $('.modal-footer').html('<button class="btn btn-danger" onclick="goBack()">Go Back</button><button id="yes" class="btn btn-success" onclick="yesClick()">Yes</button>');
 
 }
 
+function goBack() {
 
+    $('.modal-footer').html('<button id="choose" type="button" class="btn btn-success">Next</button>');
+    $('#choose').click(function () {nextSlide(coursesGlobal.courses[$('#courses-select option:selected').val()],
+        locationGlobal)});
+    $('.modal-body').html('<select name="courses" id="courses-select"></select>');
+    for(var courses = 0; courses < coursesGlobal.courses.length; courses++) {
+        $('#courses-select').append('<option value="' + courses + '">'+ coursesGlobal.courses[courses].name + '</option>');
+    }
+}
 function startCourse() {
-    window.open('showCourse.html')
+    window.open('showCourse.html');
+}
+function yesClick() {
+    $( '.modal-body' ).html('<h1>' + globalCourse.name +
+        '</h1><hr class="style18">');
+    $.ajax({
+        url: 'http://golf-courses-api.herokuapp.com/courses/' + globalCourse.id,
+        type: 'GET',
+        success: function (results) {
+
+            console.log('get Course results: ');
+            console.log(results);
+            results = JSON.parse(results);
+
+            $('#yes')[0].innerText = 'Done';
+            $('#yes').attr('onclick', 'startCourse()');
+            $("body").data( "course", results);     //send response to new page
+
+                                                                            //player Div
+            $('.modal-body').append('<div class="player"></div>');
+            $('.player').append('<span class="player-name">Player 1</span> <select name="courses" class="tee-type-select"></select>');
+
+                                                                            //send player div
+
+            for(var i = 0; i < results.course.holes[0].tee_boxes.length; i++) {
+                if(results.course.holes[0].tee_boxes[i].tee_type != 'auto change location')
+                 $('.tee-type-select').append('<option value="' + i + '">'+ results.course.holes[0].tee_boxes[i].tee_type + '</option>');
+            }
+        }
+    });
 }
 
+function weatherAPI() {
+    $.ajax({
+        crossDomain: true,
+       type: 'GET',
+        url: 'http://api.wunderground.com/api/5a97c242b3faac59/forecast/q/' + locationGlobal.lat() + ',' + locationGlobal.lng() + '.json',
+        success: function (data) {
+            console.log(data);
+            $( '.modal-body h1' ).append('<img src="' + data.forecast.simpleforecast.forecastday[0].icon_url + '">');
+        }
+    });
+}
 
--- src/js/showCourse.js
+++ src/js/showCourse.js
@@ -2,7 +2,53 @@ var userData = window.opener.$('body').data("userData");
 
 $(window).load(function() {
 
-console.log(userData);
-    $( '#myModal' ).modal('show');
+
+    makeTable();
+
 });
+
+function makeTable() {
+    tableBody1 = $('#table1-body');
+    tableBody2 = $('#table2-body');
+
+    for(var rows = 1; rows <= 3; rows++) {
+
+        var color = 'black';
+
+        switch(rows) {
+            case 1:
+                color = 'Blue';
+                break;
+            case 2:
+                color = 'White';
+                break;
+            case 3:
+                color = 'Red';
+                break;
+        }
+        tableBody1.append('<tr id="row-a-' + rows + '" style="background-color: ' + color + '"><th>' + color + '</th></tr>');
+        tableBody2.append('<tr id="row-b-' + rows + '" style="background-color: ' + color + '"><th>' + color + '</th></tr>');
+
+        for (var i = 0; i < userData.course.holes.length - 9; i++) {
+            $('#row-a-' + rows).append('<td>' + userData.course.holes[i].tee_boxes[rows].yards + '</td>');
+            $('#row-b-' + rows).append('<td>' + userData.course.holes[i+9].tee_boxes[rows].yards + '</td>');
+        }
+        $('#row-a-' + rows).append('<td></td>');
+        $('#row-b-' + rows).append('<td></td>');
+    }
+    $('#row-a-2, #row-b-2').css('color', 'black');
+
+    color = '#8f470a';
+    tableBody1.append('<tr id="row-par-a" style="background-color: ' + color + '"><th>Par</th></tr>');
+    tableBody2.append('<tr id="row-par-b" style="background-color: ' + color + '"><th>Par</th></tr>');
+
+    for(var parHole = 0; parHole < userData.course.holes.length - 9; parHole++) {
+        $('#row-par-a').append('<td>' + userData.course.holes[parHole].tee_boxes[0].par + '</td>');
+        $('#row-par-b').append('<td>' + userData.course.holes[parHole+9].tee_boxes[0].par + '</td>');
+
+    }
+    $('#row-par-a').append('<td></td>');
+    $('#row-par-b').append('<td></td>');
+
+}
 
